c845117d4c30c4061d66b64cb5647f7dea17be9c
Elisa <elisa.torazza@gmail.com>
2020-09-04 17:27:28 +0200

    Controllo

--- js/script.js
+++ js/script.js
@@ -10,14 +10,24 @@ $(document).ready(function(){
   // Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
   setTimeout(function(){
     var listaNumUtente = [];
+
     console.log(listaNumUtente);
     for (var i = 0; i<5; i++) {
     var numeroUtente = parseInt(prompt("Inserisci uno dei cinque numeri"));
     listaNumUtente.push(numeroUtente);
   }
-}, 3000);
+
   // Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
+  var listaControllo = [];
+  console.log(listaControllo);
 
-
+  for (var i = 0; i<5; i++) {
+    var trovato = false;
+    if (listaNumUtente[i] == listaNumeri[i]) {
+      trovato = true;
+      listaControllo.push(listaNumeri[i]);
+    }
+  }}, 3000);
+console.log(listaControllo);
 
 });
